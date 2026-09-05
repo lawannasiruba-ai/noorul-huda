f6dfa956bc21dc7c9f2dd90fa1f547cdd4c45dcb
lawannasiruba-ai <lawannasiruba@gmail.com>
2026-09-06 00:14:41 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "noorul-huda-v1";
+
+const FILES_TO_CACHE = [
+    "../index.html",
+    "../style.css",
+    "../script.js"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
